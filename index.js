d6959e198665f7335c8d614879afc28624d3f08c
Oksana <fdpodocs@gmail.com>
2025-07-25 13:26:11 +0300

    fix burger v1

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function contactUs() {
     alert("Спасибо за интерес! Мы свяжемся с вами в ближайшее время.");
 }
+
+function openMenu() {
+    const burger = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('#nav-links');
+    const close = document.querySelector('.close');
+
+    burger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+    close.classList.toggle('active');
+}
