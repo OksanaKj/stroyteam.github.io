bf21b352c8ecd49a26de4f2bc14a3d72add4eb61
Oksana <fdpodocs@gmail.com>
2025-07-25 17:04:43 +0300

    fix header scrolling

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function openMenu() {
     navLinks.classList.toggle('active');
     close.classList.toggle('active');
 }
+//  // Функция для проверки скролла
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('header');
+//     const secondSection = document.querySelector('hero');
+
+//     if (window.scrollY > secondSection.offsetTop) {
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled');
+//     }
+// });
